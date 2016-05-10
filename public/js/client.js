6a83713a4fa455d49e7d903fac4b2bb117ff8b7f
AlistairHopkins92 <alistairhopkins92@gmail.com>
2016-05-10 16:14:01 +0100

    Working on stopping the grey area on NS of the map

--- public/js/client.js
+++ public/js/client.js
@@ -18,7 +18,7 @@ SunApp.setToken = function(token){
 
 SunApp.saveTokenIfPresent = function(data){
   if (data.token) return this.setToken(data.token)
-  return false;
+    return false;
 }
 
 SunApp.setRequestHeader = function(xhr, settings){
@@ -82,16 +82,16 @@ SunApp.displayUsers = function(data){
   console.log("displayUsers");
   return $.each(data.users, function(index, user) {
     $(".users").prepend('<div class="media">' +
-                          '<div class="media-left">' +
-                            '<a href="#">' +
-                              '<img class="media-object" src="' + user.image +'">' +
-                            '</a>' +
-                          '</div>' +
-                          '<div class="media-body">' +
-                            '<h4 class="media-heading">@' + user.username + '</h4>' +
-                            '<p>' + user.firstName + '</p>'+
-                          '</div>' +
-                        '</div>');
+      '<div class="media-left">' +
+      '<a href="#">' +
+      '<img class="media-object" src="' + user.image +'">' +
+      '</a>' +
+      '</div>' +
+      '<div class="media-body">' +
+      '<h4 class="media-heading">@' + user.username + '</h4>' +
+      '<p>' + user.firstName + '</p>'+
+      '</div>' +
+      '</div>');
   });
 }
 
@@ -171,19 +171,38 @@ SunApp.getCities = function() {
   return SunApp.ajaxRequest("GET", "/cities", null, SunApp.loopThroughCities)
 }
 
+
+
+SunApp.limiter = function(){
+  var map = this.map;
+  google.maps.event.addListener(map, "drag", function() {
+    var proj = map.getProjection();
+    var bounds = map.getBounds();
+    var sLat = map.getBounds().getSouthWest().lat();
+    var nLat = map.getBounds().getNorthEast().lat();
+    if (sLat < -85 || nLat > 85) {
+      console.log("too much grey!!!!!!!");
+      map.setCenter(new google.maps.LatLng(0, 11.15))
+    }
+  })
+}
+
 SunApp.createWorldMap = function() {
   this.canvas = document.getElementById("map-canvas");
 
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(23.497885, 58.502197),
+    zoom: 2,
+    minZoom: 2,
+    maxZoom: 15,
+    center: new google.maps.LatLng(0, 11.15),
     disableDefaultUI: true,
     zoomControl: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
-    styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#7dcdcd"}]}]
+    styles: [{"featureType":"landscape.natural","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"color":"#e0efef"}]},{"featureType":"poi","elementType":"geometry.fill","stylers":[{"visibility":"on"},{"hue":"#1900ff"},{"color":"#c0e8e8"}]},{"featureType":"road","elementType":"geometry","stylers":[{"lightness":100},{"visibility":"simplified"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"visibility":"on"},{"lightness":700}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#70B8B8"}]}]
   }
   this.map = new google.maps.Map(this.canvas, mapOptions);
   this.getCities();
+  this.limiter();
 }
 
 $(function(){
