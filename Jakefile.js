92ca9846dbe263009a5be4c488eb77ec195f43e8
Wardrt <ryanward1@btinternet.com>
2016-05-10 11:35:08 +0100

    All markers are showing up on the map.

--- Jakefile.js
+++ Jakefile.js
@@ -6,78 +6,78 @@ mongoose.connect(config.database);
 var City = require("./models/city");
 
 var cities = [
-  // {
-  //   name: "Paris",
-  //   latitude: 48.8589996,
-  //   longitude: 2.2071294,
-  //   continent: "Europe"
-  // },
-  // {
-  //   name: "New York",
-  //   latitude: 40.70583,
-  //   longitude: -74.2588803,
-  //   continent: "NAmerica"
-  // },
-  // {
-  //   name: "Aberdeen",
-  //   latitude: 57.149717, 
-  //   longitude: -2.094278,
-  //   continent: "Europe"
-  // },
-  // {
-  //   name: "Marrakech",
-  //   latitude: 31.629472, 
-  //   longitude: -7.981084,
-  //   continent: "Africa"
-  // },
-  // {
-  //   name: "Athens",
-  //   latitude: 37.983917, 
-  //   longitude: 23.72936,
-  //   continent: "Europe"
-  // },
-  // {
-  //   name: "Bali",
-  //   latitude: -8.696808,
-  //   longitude: 115.208817,
-  //   continent: "Asia"
-  // },
-  // {
-  //   name: "Singapore",
-  //   latitude: 1.302755,
-  //   longitude: 103.862686,
-  //   continent: "Asia"
-  // },
-  // {
-  //   name: "Columbo",
-  //   latitude: 6.908321,
-  //   longitude: 79.867172,
-  //   continent: "Asia"
-  // },
-  // {
-  //   name: "Sydney",
-  //   latitude: -33.923492,
-  //   longitude: 151.225433,
-  //   continent: "Australasia"
-  // },
-  // {
-  //   name: "Tokyo",
-  //   latitude: 35.702718,
-  //   longitude: 139.733047,
-  //   continent: "Asia"
-  // },
-  // {
-  //   name: "Cape Town",
-  //   latitude: -33.931557,
-  //   longitude: 18.467674,
-  //   continent: "Africa"
-  // },
-  // {
-  //   name: "Brasilia",
-  //   latitude: -15.837508,
-  //   longitude: -47.872925,
-  //   continent: "SAmerica"
-  // },
+  {
+    name: "Paris",
+    latitude: 48.8589996,
+    longitude: 2.2071294,
+    continent: "Europe"
+  },
+  {
+    name: "New York",
+    latitude: 40.70583,
+    longitude: -74.2588803,
+    continent: "NAmerica"
+  },
+  {
+    name: "Aberdeen",
+    latitude: 57.149717, 
+    longitude: -2.094278,
+    continent: "Europe"
+  },
+  {
+    name: "Marrakech",
+    latitude: 31.629472, 
+    longitude: -7.981084,
+    continent: "Africa"
+  },
+  {
+    name: "Athens",
+    latitude: 37.983917, 
+    longitude: 23.72936,
+    continent: "Europe"
+  },
+  {
+    name: "Bali",
+    latitude: -8.696808,
+    longitude: 115.208817,
+    continent: "Asia"
+  },
+  {
+    name: "Singapore",
+    latitude: 1.302755,
+    longitude: 103.862686,
+    continent: "Asia"
+  },
+  {
+    name: "Columbo",
+    latitude: 6.908321,
+    longitude: 79.867172,
+    continent: "Asia"
+  },
+  {
+    name: "Sydney",
+    latitude: -33.923492,
+    longitude: 151.225433,
+    continent: "Australasia"
+  },
+  {
+    name: "Tokyo",
+    latitude: 35.702718,
+    longitude: 139.733047,
+    continent: "Asia"
+  },
+  {
+    name: "Cape Town",
+    latitude: -33.931557,
+    longitude: 18.467674,
+    continent: "Africa"
+  },
+  {
+    name: "Brasilia",
+    latitude: -15.837508,
+    longitude: -47.872925,
+    continent: "SAmerica"
+  },
   {
     name: "Muscat",
     latitude: 23.497885, 
