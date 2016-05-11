45fe47c51f16ad19703549e4c3c5144893680b2b
matmenzl <info@mathiasmenzl.ch>
2016-05-11 11:37:42 +0100

    Changing mongolab_uri to mongodb_uri

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   port: process.env.PORT || 3000,
-  database: process.env.MONGOLAB_URI || "mongodb://localhost/passport-jwt-boilerplate",
+  database: process.env.MONGODB_URI || "mongodb://localhost/passport-jwt-boilerplate",
   secret: process.env.SECRET || "Acjhw338tw0@E*¢®^€∆sw2"
 }
