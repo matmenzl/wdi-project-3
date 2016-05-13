f6c94a035ea9ac2f1403cb25076639b9fb44d1ba
Wardrt <ryanward1@btinternet.com>
2016-05-13 11:39:21 +0100

    Final touches.

--- public/js/client.js
+++ public/js/client.js
@@ -203,7 +203,7 @@ SunApp.addInfoWindowForCity = function(city, marker){
     if(typeof self.infowindow != "undefined") self.infowindow.close();
 
     self.infowindow = new google.maps.InfoWindow({
-      content: "<h1 id='title'>"+city.name+"</h1><p id='summary'>"+city.summary+"</p><div id='snippet_searchpanel' style='width: auto; height:auto;'></div></br><button id='favourite-button' class='btn btn-primary'>favourite</button>"
+      content: "<div id='snippet_searchpanel' style='width: auto; height:auto;'></div></br><div id='center-button'><button id='favourite-button' class='btn btn-primary'>favourite</button></div>"
     });
 
     google.maps.event.addListener(self.infowindow, 'domready', function() {
@@ -265,7 +265,7 @@ SunApp.createWorldMap = function() {
     maxZoom: 15,
     center: new google.maps.LatLng(0, 11.15),
     disableDefaultUI: true,
-    zoomControl: false,
+    zoomControl: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#D5E2EA"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#e2ead5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
   }
@@ -284,7 +284,7 @@ SunApp.createCityMap = function(cityId) {
     maxZoom: 15,
     center: new google.maps.LatLng(0, 11.15),
     disableDefaultUI: true,
-    zoomControl: false,
+    zoomControl: true,
     mapTypeId: google.maps.MapTypeId.ROADMAP,
     styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#e9e9e9"},{"lightness":17}]},{"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":20}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffffff"},{"lightness":17}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#ffffff"},{"lightness":29},{"weight":0.2}]},{"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":18}]},{"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#ffffff"},{"lightness":16}]},{"featureType":"poi","elementType":"geometry","stylers":[{"color":"#f5f5f5"},{"lightness":21}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#dedede"},{"lightness":21}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#ffffff"},{"lightness":16}]},{"elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#333333"},{"lightness":40}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f2f2f2"},{"lightness":19}]},{"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fefefe"},{"lightness":20}]},{"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fefefe"},{"lightness":17},{"weight":1.2}]}]
   }
